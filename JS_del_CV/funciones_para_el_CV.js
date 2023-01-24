9a69e4ba66a49b41c60193b3b070b89aa8c41e8d
Lucas-Gagod <109626492+Lucas-Gagod@users.noreply.github.com>
2023-01-24 20:47:00 -0300

    Primer Repositorio

---
+++ JS_del_CV/funciones_para_el_CV.js
@@ -0,0 +1,7 @@
+
+document.getElementById('boton') .addEventListener('click', function(){
+    document.getElementById("demos").innerHTML = "Ademas de Hablar Español, puedo comunicarme perfectamente en Ingles y Frances. Tambien estoy capacitado para el desarrollo de sitios web, por medio de HTML, Java Script y CSS";
+})
+document.getElementById('boton2') .addEventListener('click', function(){
+    document.getElementById("demos").innerHTML = "Trabaje en distintas Empresas como Microsoft, Java y Google como Analista Programador y Analista en Sistemas de Información, lo cual me otorgo mucha experiencia en un ambiente bastante organizado.";
+})
